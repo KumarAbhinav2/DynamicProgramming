6281bd37d15e809f663641fc901d681b9e974b25
Abhinav <er.abhi01@gmail.com>
2020-09-21 15:31:08 +0530

    adds another one

---
+++ coin_problem.py
@@ -0,0 +1,29 @@
+"""
+Given a value N, if we want to make change for N cents, and we have infinite supply of each of S = { S1, S2, .. , Sm}
+valued coins, how many ways can we make the change? The order of coins doesn’t matter.
+
+For example, for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}. So output should be 4.
+For N = 10 and S = {2, 5, 3, 6}, there are five solutions: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}.
+So the output should be 5.
+"""
+
+
+class Solution:
+
+    def change(self, N, coins):
+        dp = [0] * (N + 1)
+        dp[0] = 1
+        for i in coins:
+            for j in range(1, N + 1):
+                if j >= i:
+                    dp[j] += dp[j - i]
+        print(dp[N])
+
+
+obj = Solution()
+obj.change(5, [1, 2, 5])
+
+
+
+
+
